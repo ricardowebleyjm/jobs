d2c26b629d3531ab7c26c75be968057d8e8daa37
Ricardo <Ricardo.Webley@integrity.gov.jm>
2022-12-29 11:50:21 -0500

    Added custom user app

--- jobs/settings.py
+++ jobs/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
     'rest_framework',
 ]
 
@@ -122,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom User
+AUTH_USER_MODEL = 'users.User'
